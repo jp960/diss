bdb5339fba7cfdcb4699e7928405da7309f82639
janhavi <jp960@bath.ac.uk>
2019-02-17 18:49:15 +0000

    fixing model

--- model.py
+++ model.py
@@ -67,8 +67,7 @@ class pix2pix(object):
 
     def build_model(self):
         self.real_data = tf.placeholder(tf.float32,
-                                        [self.batch_size, 2, self.image_size, self.image_size,
-                                         self.input_c_dim + self.output_c_dim],
+                                        [self.batch_size, 2, self.image_size, self.image_size],
                                         name='real_A_and_B_images')
         print("*" * 40)
         print(self.real_data)
@@ -76,7 +75,7 @@ class pix2pix(object):
         print("image size " + str(self.image_size))
         print("input dim " + str(self.input_c_dim))
         print("output " + str(self.output_c_dim))
-        print(self.real_data[:, :, :, :self.input_c_dim])
+        print(self.real_data[:, :, :, ])
         print(self.real_data[:, :, :, self.input_c_dim:self.input_c_dim + self.output_c_dim])
         print("*" * 40)
 
