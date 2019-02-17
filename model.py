39fbf67541aec324208925ce80d3a7172512e252
janhavi <jp960@bath.ac.uk>
2019-02-17 18:06:33 +0000

    fixing model

--- model.py
+++ model.py
@@ -140,6 +140,7 @@ class pix2pix(object):
                 print("here")
                 batch_files = data[idx*self.batch_size:(idx+1)*self.batch_size]
                 batch = [load_data(batch_file[0], batch_file[1]) for batch_file in batch_files]
+                print(self.is_grayscale)
                 if (self.is_grayscale):
                     batch_images = np.array(batch).astype(np.float32)[:, :, :, None]
                 else:
