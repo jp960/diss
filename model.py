637d5849e271606426ba0f2bca5c1975358c7719
jp960 <jp960@bath.ac.uk>
2019-03-25 16:04:01 +0000

    fixing model

--- model.py
+++ model.py
@@ -66,7 +66,7 @@ class pix2pix(object):
 
     def build_model(self):
         self.real_data = tf.placeholder(tf.float32,
-                                        [self.batch_size, self.image_size * 2, self.image_size, 1],
+                                        [None, self.image_size * 2, self.image_size, 1],
                                         name='real_A_and_B_images')
 
         self.real_preprocessed = self.real_data[:, :256, :, :]
@@ -84,8 +84,8 @@ class pix2pix(object):
         self.saver = tf.train.Saver()
 
     def load_random_samples(self):
-        preprocessed_data = np.random.choice(glob('/home/janhavi/Documents/diss/NYU/preprocessed/*.png'),
-                                             self.batch_size)
+        preprocessed_data = np.random.choice(glob('/home/janhavi/Documents/diss/practise10/preprocessed/*.png'),
+                                             self.sample_size)
         depth_data = [path.replace('preprocessed', 'depths') for path in preprocessed_data]
         depth_images_raw = [load_image(path) for path in depth_data]
         data = list(zip(preprocessed_data, depth_data))
@@ -105,9 +105,9 @@ class pix2pix(object):
             [self.output, self.g_loss],
             feed_dict={self.real_data: sample_images}
         )
-        save_images(samples, depth_images, [self.batch_size, 1],
-                    '/home/janhavi/Documents/diss/train/output1449_0001/train_{0}_{1}.png'.format(epoch, int(g_loss)),
-                    '/home/janhavi/Documents/diss/train/output1449_0001/train_{0}_{1}.txt'.format(epoch, int(g_loss)))
+        save_images(samples, depth_images, [self.sample_size, 1],
+                    '/home/janhavi/Documents/diss/train/output10/train_{0}_{1}.png'.format(epoch, int(g_loss)),
+                    '/home/janhavi/Documents/diss/train/output10/train_{0}_{1}.txt'.format(epoch, int(g_loss)))
         print("[Sample] g_loss: {:.8f}".format(g_loss))
 
     def train(self, args):
@@ -129,9 +129,10 @@ class pix2pix(object):
             print(" [!] Load failed...")
 
         for epoch in xrange(args.epoch):
-            data_pre = sorted(glob('/home/janhavi/Documents/diss/NYU/preprocessed/*.png'))
-            data_depth = sorted(glob('/home/janhavi/Documents/diss/NYU/depths/*.png'))
+            data_pre = sorted(glob('/home/janhavi/Documents/diss/practise10/preprocessed/*.png'))
+            data_depth = sorted(glob('/home/janhavi/Documents/diss/practise10/depths/*.png'))
             data = list(zip(data_pre, data_depth))
+
             batch_idxs = min(len(data), args.train_size) // self.batch_size
             for idx in xrange(0, batch_idxs):
                 batch_files = data[idx * self.batch_size:(idx + 1) * self.batch_size]
@@ -149,7 +150,7 @@ class pix2pix(object):
                 print("Epoch: [%2d] [%4d/%4d] time: %4.4f, g_loss: %.8f" \
                       % (epoch, idx, batch_idxs, time.time() - start_time, errG))
 
-                if np.mod(counter, 100) == 1:  # change back to 100
+                if np.mod(counter, 5) == 1:  # change back to 100
                     self.sample_model(args.sample_dir, epoch, idx)
                     break
 
@@ -157,6 +158,7 @@ class pix2pix(object):
                     self.save(args.checkpoint_dir, counter)
 
     def generator(self, image, y=None):
+        batch_size = tf.shape(image)[0]
         with tf.variable_scope("generator") as scope:
             s = self.output_size
             s2, s4, s8, s16, s32, s64, s128 = int(s / 2), int(s / 4), int(s / 8), int(s / 16), int(s / 32), int(
@@ -181,55 +183,55 @@ class pix2pix(object):
             # e8 is (1 x 1 x self.gf_dim*8)
 
             self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
-                                                     [self.batch_size, s128, s128, self.gf_dim * 8], name='g_d1',
+                                                     [batch_size, s128, s128, self.gf_dim * 8], name='g_d1',
                                                      with_w=True)
             d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
             d1 = tf.concat([d1, e7], 3)
             # d1 is (2 x 2 x self.gf_dim*8*2)
 
             self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
-                                                     [self.batch_size, s64, s64, self.gf_dim * 8], name='g_d2',
+                                                     [batch_size, s64, s64, self.gf_dim * 8], name='g_d2',
                                                      with_w=True)
             d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
             d2 = tf.concat([d2, e6], 3)
             # d2 is (4 x 4 x self.gf_dim*8*2)
 
             self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
-                                                     [self.batch_size, s32, s32, self.gf_dim * 8], name='g_d3',
+                                                     [batch_size, s32, s32, self.gf_dim * 8], name='g_d3',
                                                      with_w=True)
             d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
             d3 = tf.concat([d3, e5], 3)
             # d3 is (8 x 8 x self.gf_dim*8*2)
 
             self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
-                                                     [self.batch_size, s16, s16, self.gf_dim * 8], name='g_d4',
+                                                     [batch_size, s16, s16, self.gf_dim * 8], name='g_d4',
                                                      with_w=True)
             d4 = self.g_bn_d4(self.d4)
             d4 = tf.concat([d4, e4], 3)
             # d4 is (16 x 16 x self.gf_dim*8*2)
 
             self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
-                                                     [self.batch_size, s8, s8, self.gf_dim * 4], name='g_d5',
+                                                     [batch_size, s8, s8, self.gf_dim * 4], name='g_d5',
                                                      with_w=True)
             d5 = self.g_bn_d5(self.d5)
             d5 = tf.concat([d5, e3], 3)
             # d5 is (32 x 32 x self.gf_dim*4*2)
 
             self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
-                                                     [self.batch_size, s4, s4, self.gf_dim * 2], name='g_d6',
+                                                     [batch_size, s4, s4, self.gf_dim * 2], name='g_d6',
                                                      with_w=True)
             d6 = self.g_bn_d6(self.d6)
             d6 = tf.concat([d6, e2], 3)
             # d6 is (64 x 64 x self.gf_dim*2*2)
 
             self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
-                                                     [self.batch_size, s2, s2, self.gf_dim], name='g_d7', with_w=True)
+                                                     [batch_size, s2, s2, self.gf_dim], name='g_d7', with_w=True)
             d7 = self.g_bn_d7(self.d7)
             d7 = tf.concat([d7, e1], 3)
             # d7 is (128 x 128 x self.gf_dim*1*2)
 
             self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
-                                                     [self.batch_size, s, s, self.output_c_dim], name='g_d8',
+                                                     [batch_size, s, s, self.output_c_dim], name='g_d8',
                                                      with_w=True)
             # d8 is (256 x 256 x output_c_dim)
 
